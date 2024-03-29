4cbdc679fd342f532c0093b645bcc816474b0c10
Luc Verdier <luc.verdier@dougs.fr>
2024-03-29 13:45:28 +0100

    add title and meta

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
+<script setup lang="ts">
+useHead({
+  title: 'Dogm.io - Share your design knowledge and resources from Figma',
+  meta: [
+    { name: 'description', content: 'The easiest way to share your design knowledge and resources from Figma' }
+  ],
+})
+</script>
+
 <template>
   <div class="container mx-auto relative z-10">
-    <NuxtPage /> 
+    <NuxtPage />
   </div>
-  <Background/>
+  <Background />
 </template>
