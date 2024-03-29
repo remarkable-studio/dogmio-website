76712996ad24369d92437ad92955a799018c8699
Mickaël Marquez <marquez.mickael@gmail.com>
2024-03-29 12:01:46 +0100

    confirmation page

--- pages/confirmation.vue
+++ pages/confirmation.vue
@@ -1,3 +1,11 @@
 <template>
-  <h1>Confirmation</h1>
+  <div class="max-w-xl mx-auto p-6 h-screen flex flex-col justify-center">
+    <div class="flex flex-col gap-6 items-center">
+      <div>
+        <h1 class="text-center h1 text-neutral-hightest mb-2">Welcome to the Dogm.io Waitlist!</h1>
+        <p class="text-center text-small text-neutral-hightest/50">You're on your way to sharing your design knowledge and resources like never before. Stay tuned for updates and your exclusive access.</p>
+        <p class="text-center text-small text-neutral-hightest/50">We're excited to have you with us!</p>
+      </div>
+    </div>
+  </div>
 </template>
